2e21b67bdbeca9c9b7905f48b6a310880415e419
patrickeleonard <peleonard12@gmail.com>
2015-04-27 00:04:01 -0700

    ran the mlb 2014 a lot

---
+++ mlb2014.py
@@ -0,0 +1,45 @@
+#scoped for just 2014 ()
+def setDivisions():
+
+    divisions = {
+        'Orioles': 'AL East',
+        'Red Sox': 'AL East',
+        'Yankees': 'AL East', 
+        'Rays': 'AL East',
+        'Blue Jays': 'AL East',
+
+        'White Sox': 'AL Central',
+        'Indians': 'AL Central',
+        'Tigers': 'AL Central',
+        'Royals': 'AL Central',
+        'Twins': 'AL Central',
+
+        'Astros': 'AL West',
+        'Angels': 'AL West',
+        'Athletics': 'AL West',
+        'Mariners': 'AL West',
+        'Rangers': 'AL West',
+
+        'Braves': 'NL East',
+        'Marlins': 'NL East',
+        'Mets': 'NL East',
+        'Phillies': 'NL East',
+        'Nationals': 'NL East',
+
+        'Cubs': 'NL Central',
+        'Reds': 'NL Central',
+        'Brewers': 'NL Central',
+        'Pirates': 'NL Central',
+        'Cardinals': 'NL Central',
+
+        'Diamondbacks': 'NL West',
+        'Rockies': 'NL West',
+        'Dodgers': 'NL West',
+        'Padres': 'NL West',
+        'Giants': 'NL West'
+        }
+
+    return divisions
+
+
+    
--- playoffs.py
+++ playoffs.py
@@ -2,68 +2,17 @@ from sys import argv
 from random import seed, random
 from math import pow
 
-
 fileName = argv[1]
+numTrials = int(argv[2])
 
 
-class Team:
-
-    def __init__(self, teamData):
-        self.seed = int(teamData[0])
-        self.name = teamData[1]
-        self.rwins = int(teamData[2])
-        self.rlosses = int(teamData[3])
-
-        [self.league, self.division] = DivisionsMLB[self.name].split(' ')
-
-        #print self.seed, self.name, self.wins, self.losses, self.league, self.division
-
-
-
-
-DivisionsMLB = {
-    'Orioles': 'AL East',
-    'Red Sox': 'AL East',
-    'Yankees': 'AL East', 
-    'Rays': 'AL East',
-    'Blue Jays': 'AL East',
-
-    'White Sox': 'AL Central',
-    'Indians': 'AL Central',
-    'Tigers': 'AL Central',
-    'Royals': 'AL Central',
-    'Twins': 'AL Central',
-
-    'Astros': 'AL West',
-    'Angels': 'AL West',
-    'Athletics': 'AL West',
-    'Mariners': 'AL West',
-    'Rangers': 'AL West',
-
-    'Braves': 'NL East',
-    'Marlins': 'NL East',
-    'Mets': 'NL East',
-    'Phillies': 'NL East',
-    'Nationals': 'NL East',
-
-    'Cubs': 'NL Central',
-    'Reds': 'NL Central',
-    'Brewers': 'NL Central',
-    'Pirates': 'NL Central',
-    'Cardinals': 'NL Central',
-
-    'Diamondbacks': 'NL West',
-    'Rockies': 'NL West',
-    'Dodgers': 'NL West',
-    'Padres': 'NL West',
-    'Giants': 'NL West'
-    }
-
-
+#I think this should conceivably work for any team, in any league, in any year
 def playSeries(team1, team2, numGames):
-#teams 1 and 2 are Team objects, numGames is the maximum series length
+    
+    #teams 1 and 2 are Team objects, numGames is the maximum series length
 
-    gameThresh = pow(team1.rwins, 2) / (pow(team1.rwins, 2) + pow(team2.rwins, 2))
+    #threshold set from a Bill James-style pythagorean formula, based on points from Team objects
+    gameThresh = pow(team1.points, 2) / (pow(team1.points, 2) + pow(team2.points, 2))
 
     winsNeeded = (numGames + 1)/2
 
@@ -88,15 +37,89 @@ def playSeries(team1, team2, numGames):
         currentGame += 1
 
 
+#scoped to overall MLB level
+class Team:
 
+    def __init__(self, teamData):
+
+        global divisions
+
+        self.seed = int(teamData[0])
+        self.name = teamData[1]
+        self.rwins = int(teamData[2]) #regular season wins
+        self.rlosses = int(teamData[3]) #regular season losses
+
+        if len(teamData) > 4:
+            self.rties = int(teamData[4]) #ties, if applicable (overtime losses in hockey)
+
+
+        [self.league, self.division] = divisions[self.name].split(' ')
+
+        self.points = self.rwins #in MLB, a team's "points" is given by regular season wins
+
+        #print self.seed, self.name, self.wins, self.losses, self.league, self.division
+
+
+def runPlayoffs(seedsAL, seedsNL):
+
+    # WILD CARD GAMES
+    wcAL = playSeries(seedsAL[4], seedsAL[5], 1)
+    wcNL = playSeries(seedsNL[4], seedsNL[5], 1)
+
+    # DIVISION SERIES
+    alcs1 = playSeries(seedsAL[1], wcAL, 5)
+    alcs2 = playSeries(seedsAL[2], seedsAL[3], 5)
+
+    nlcs1 = playSeries(seedsNL[1], wcNL, 5)
+    nlcs2 = playSeries(seedsNL[2], seedsNL[3], 5)
+
+    # LEAGUE CHAMPIONSHIP SERIES
+    champAL = playSeries(alcs1, alcs2, 7)
+    champNL = playSeries(nlcs1, nlcs2, 7)
+
+    # WORLD SERIES
+    champWS = playSeries(champAL, champNL, 7)
+
+    # Print statements
+    # print "\nWild Card winners:"
+    # print wcAL.name
+    # print wcNL.name, "\n"
+
+    # print "ALDS winners:"
+    # print alcs1.name
+    # print alcs2.name, "\n"
+
+    # print "NLDS winners:"
+    # print nlcs1.name
+    # print nlcs2.name, "\n"
+
+    # print "League champions:"
+    # print champAL.name
+    # print champNL.name, "\n"
+
+    # print "World Series champions:"
+    # print champWS.name, "\n"
+
+    return champWS
 
 
 #MAIN FUNCTION
+
+from mlb2014 import setDivisions
+
+global divisions
+divisions = setDivisions()
+
+# print divisions
+
+seedsAL = {}
+seedsNL = {}
+
+totalWins = {}
+
+
 with open(fileName, 'r') as teamsFile:
 
-    teamsList = []
-    seedsAL = {}
-    seedsNL = {}
 
     for line in teamsFile:
 
@@ -111,58 +134,28 @@ with open(fileName, 'r') as teamsFile:
         elif currentTeam.league == 'NL':
             seedsNL[currentTeam.seed] = currentTeam
 
-        teamsList.append(currentTeam)
+        totalWins[currentTeam.name] = 0
 
-    # DEBUGGING
-
-    # for team in teamsList:
-    #     print team.name
-
-    # print "AL: ", seedsAL
-    # print "NL: ", seedsNL
-
-    # for i in range(1,6):
-    #     print "%d:" % i, seedsAL[i].name
-    # for i in range(1,6):
-    #     print "%d:" % i, seedsNL[i].name
+    # print "AL seeds: ", seedsAL
+    # print "NL seeds: ", seedsNL
 
 
-    # WILD CARD GAMES
-    wcAL = playSeries(seedsAL[4], seedsAL[5], 1)
-    wcNL = playSeries(seedsNL[4], seedsNL[5], 1)
+# SIMULATING A FUCKLOAD OF PLAYOFFS
+for i in range(0,numTrials):
 
-    print "\nWild Card winners:"
-    print wcAL.name
-    print wcNL.name, "\n"
+    winner = runPlayoffs(seedsAL, seedsNL)
+
+    totalWins[winner.name] += 1
 
 
-    # DIVISION SERIES
 
-    alcs1 = playSeries(seedsAL[1], wcAL, 5)
-    alcs2 = playSeries(seedsAL[2], seedsAL[3], 5)
+# PRINTING RESULTS
+print "\nWorld Series Championships (%d Trials):\n" % numTrials 
 
-    nlcs1 = playSeries(seedsNL[1], wcNL, 5)
-    nlcs2 = playSeries(seedsNL[2], seedsNL[3], 5)
+for team in totalWins:
 
-    print "ALDS winners:"
-    print alcs1.name
-    print alcs2.name, "\n"
+    print team, totalWins[team], "\n"
 
-    print "NLDS winners:"
-    print nlcs1.name
-    print nlcs2.name, "\n"
-
-    champAL = playSeries(alcs1, alcs2, 7)
-    champNL = playSeries(nlcs1, nlcs2, 7)
-
-    print "League champions:"
-    print champAL.name
-    print champNL.name, "\n"
-
-    champWS = playSeries(champAL, champNL, 7)
-
-    print "World Series champions:"
-    print champWS.name, "\n"
 
 
 
